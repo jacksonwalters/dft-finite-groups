f8adf3f932ee762ae6eff4f099024c3401ebca5d
Jackson Walters <jacksonwalters@gmail.com>
2025-03-17 23:47:40 -0400

    move string conversion function to io_utils

--- symmetric_group/unitary_dft_finite_fields/io_utils.py
+++ symmetric_group/unitary_dft_finite_fields/io_utils.py
@@ -40,3 +40,29 @@ def load_array(path, filename):
     with open(full_path, 'r') as f:
         line = f.readline().strip()
         return line.split(",")
+    
+#given a list of eigenvalues in string representation, convert to elements of algebraic closure
+import re
+def str_repn_to_alg_closure(F_bar, eigenvalues_F_bar_list):
+    eigenvalues_F_bar = []
+    for eig in eigenvalues_F_bar_list:
+        monomials = eig.split('+')
+        pattern = r'(\d*)\*?z(\d+)(\^(\d+))?|\b(\d+)\b'
+        eig_F_bar = 0
+        for monomial in monomials:
+            match = re.match(pattern, monomial.strip())
+            if match:
+                if match.group(5):  # Case for constants (no 'z' part)
+                    coefficient = int(match.group(5))
+                    variable_number = 1
+                    exponent = 0
+                else:  # Case for terms with 'z' and potentially an exponent
+                    coefficient = int(match.group(1)) if match.group(1) else 1  # Default to 1 if no coefficient
+                    variable_number = int(match.group(2))  # The integer after 'z'
+                    exponent = int(match.group(4)) if match.group(4) else 1  # Default to 1 if no exponent
+                monomial_F_bar = F_bar(coefficient)*F_bar.gen(variable_number)**exponent
+                eig_F_bar += monomial_F_bar
+            else:
+                print(f"Monomial: {monomial.strip()} - No match found")
+        eigenvalues_F_bar.append(eig_F_bar)
+    return eigenvalues_F_bar
